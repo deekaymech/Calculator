4feaf8f3fad02f7fad7795cd3498361ee23ce42b
USER <USER@ACER>
2019-06-25 21:25:45 -0700

    First Commit

---
+++ exercise1/src/exercise2/Calculator.java
@@ -0,0 +1,49 @@
+package exercise2;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Calculator calc = new Calculator();
+	    calc.add();
+	    calc.sub();
+	    calc.mul();
+	    calc.div();
+	}
+	    void add()
+	    {
+	    	int Number1=10;
+	    	int Number2=20;
+	    	int Total=Number1+Number2;
+	    	System.out.println("Total is "+Total);
+	    
+	    }
+	    void sub()
+	    {
+	    	int Number1=10;
+	    	int Number2=20;
+	    	int Total=Number1-Number2;
+	    	System.out.println("Total is "+Total);
+	    
+	    }
+	    void mul()
+	    {
+	    	int Number1=10;
+	    	int Number2=20;
+	    	int Total=Number1*Number2;
+	    	System.out.println("Total is "+Total);
+	    
+	    }
+	    void div()
+	    {
+	    	int Number1=100;
+	    	int Number2=20;
+	    	int Total=Number1/Number2;
+	    	System.out.println("Total is "+Total);
+	    
+	    }
+	    }
+		
+		
+		
